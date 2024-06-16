6aa2fb5f500f052d639ae08c3ff4e6d3b2ff34d1
Mamed766 <mehemmeddemirli57@gmail.com>
2024-06-16 16:48:03 +0400

    post get delete

--- src/js/script.js
+++ src/js/script.js
@@ -9,6 +9,22 @@ const getApiDataWithCallBack = async (endPoint, cb) => {
   cb(response);
 };
 
+const PostApiData = async (endPoint, data) => {
+  let response = await fetch(`${BaseURL}/${endPoint}`, {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+
+  return response;
+};
+
+const DeleteApiDataById = async (endPoint, id) => {
+  let response = await fetch(`${BaseURL}/${endPoint}/${id}`, {
+    method: "DELETE",
+  });
+  return response;
+};
+
 getApiDataWithCallBack("data", (data) => {
   data.map((item) => {
     cardsContainer.innerHTML += `
@@ -21,13 +37,45 @@ getApiDataWithCallBack("data", (data) => {
             </div>
             <div class="cards__container--card__content">
               <h2>${item.city}</h2>
-              <p>${item.properties}</p>
+              <p>${item.properties} properties</p>
               <p class="explore">
                 Explore Now <i class="fa-solid fa-arrow-right"></i>
               </p>
-              <button>Delete</button>
+              <button class="delete" data-id=${item.id}>Delete</button>
             </div>
           </div>
           `;
+
+    const DELETE_BTN = document.querySelectorAll(".delete");
+
+    DELETE_BTN &&
+      DELETE_BTN.forEach((btn) =>
+        btn.addEventListener("click", (e) => {
+          e.preventDefault();
+
+          let attrId = e.target.getAttribute("data-id");
+
+          DeleteApiDataById("data", attrId);
+        })
+      );
   });
 });
+
+const imgSrc = document.querySelector("#imagesrc");
+const city = document.querySelector("#city");
+const properties = document.querySelector("#properties");
+
+const CREATE__CARD = document.querySelector("#create");
+
+CREATE__CARD &&
+  CREATE__CARD.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const cardData = {
+      imageSrc: imgSrc.value,
+      city: city.value,
+      properties: properties.value,
+    };
+
+    PostApiData("data", cardData);
+  });
